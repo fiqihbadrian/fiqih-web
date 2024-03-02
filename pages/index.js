8fdd94e8572ed2753adba36e76c8fbfeaa181b64
Fiqh Bdrn <76670473+fiqihbadrian@users.noreply.github.com>
2024-03-02 19:10:17 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Jangan Main Slot Mulu Aa!" />
         <p className="description">
           Hahahaha
         </p>
